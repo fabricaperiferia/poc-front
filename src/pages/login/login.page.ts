3a49440c41fda43c62cf64a62c2fb2e097c96015
raphael-martinez-periferia <raphael.martinez@periferia-it.com>
2019-03-25 12:32:06 -0500

    componentes

--- src/pages/login/login.page.ts
+++ src/pages/login/login.page.ts
@@ -1,6 +1,8 @@
 
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+import { User } from '../../models/user'
 @Component({
   selector: 'page-login',
   templateUrl: 'login.page.html',
@@ -8,9 +10,15 @@ import { Component } from '@angular/core';
 })
 
 export class LoginPage {
-
+  myForm: FormGroup;
+  user = {} as User;
   urlFoundImg: String = 'assets/img/fondo-login.png';
-    constructor(){
-        
-    }
+  constructor() {
+
+  }
+
+  loginApp() {
+    this.user = this.myForm.value;
+    console.log(this.user)
+  }
 }
